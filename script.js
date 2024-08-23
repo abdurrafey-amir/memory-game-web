449698ccc9e34da94f6431a9acad7b6ad895537a
abdurrafey-amir <abdurrafeyamir15@gmail.com>
2024-08-24 00:15:49 +0500

    done

--- script.js
+++ script.js
@@ -50,6 +50,8 @@ document.addEventListener('DOMContentLoaded', () => {
         }
     ]    
 
+    cardArray.sort(() => 0.5 - Math.random())
+
     const board = document.querySelector('.board')
     const score = document.querySelector('#score')
     const placeholder = 'assets/duck.png'
@@ -60,6 +62,54 @@ document.addEventListener('DOMContentLoaded', () => {
             var card = document.createElement('img')
             card.setAttribute('src', placeholder)
             card.setAttribute('data-id', i)
+            card.addEventListener('click', flipCard)
+            board.appendChild(card)
         }
     }
+    createBoard()
+
+    var cardsClicked = []
+    var cardsClickedId = []
+    var cardsMatched = []
+
+    function flipCard() {
+        var cardid = this.getAttribute('data-id')
+        cardsClicked.push(cardArray[cardid].name)
+        cardsClickedId.push(cardid)
+        this.setAttribute('src', cardArray[cardid].img)
+
+        if (cardsClicked.length === 2) {
+            setTimeout(checkMatch, 500)
+        }
+    }
+
+    function checkMatch() {
+        var cards = document.querySelectorAll('img')
+        const firstCard = cardsClickedId[0]
+        const secondCard = cardsClickedId[1]
+
+        if (firstCard === secondCard) {
+            cards[firstCard].setAttribute('src', placeholder)
+            cards[secondCard].setAttribute('src', placeholder)
+            alert('You have clicked the same card!')
+        } else if (cardsClicked[0] === cardsClicked[1]) {
+            cards[firstCard].setAttribute('src', blank)
+            cards[secondCard].setAttribute('src', blank)
+            cardsMatched.push(cardsClicked)
+            cards[firstCard].removeEventListener('click', flipCard)
+            cards[secondCard].removeEventListener('click', flipCard)
+        } else {
+            cards[firstCard].setAttribute('src', placeholder)
+            cards[secondCard].setAttribute('src', placeholder)
+        }
+        if (cardsMatched.length === cardArray.length/2) {
+            alert('You win!')
+        }
+        
+        cardsClicked = []
+        cardsClickedId = []
+        score.textContent = cardsMatched.length
+    }
+
+    
 })
